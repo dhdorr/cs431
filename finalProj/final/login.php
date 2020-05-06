0c54b964db68f62169d82b58653167c78fa1800d
yyx3333 <60845370+yyx3333@users.noreply.github.com>
2020-05-06 02:30:02 -0700

    update signup and login

--- finalProj/final/login.php
+++ finalProj/final/login.php
@@ -7,18 +7,38 @@
     <!-- Insert Navbar -->
     <?php require "header.php"; ?>
   </head>
-  <body>
-    <div class="container-fluid">
-      <div class="row justify-content-center">
-        <h2>We are logging in.</h2>
-      </div>
-      <div class="row justify-content-center">        
-        <form class="form-login" action="includes/login.inc.php" method="post">
-          <input type="text" name="username" placeholder="Username">
-          <input type="text" name="pwd" placeholder="Password">
-          <button type="submit" name="login_submit" class="btn btn-dark">LOG IN</button>
-        </form>
-      </div>
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+
+  <!-- Custom styles for this template -->
+  <link href="style.css" rel="stylesheet">
+
+
+<body class="text-center">
+  <form class="form-signin" action="includes/login.inc.php" method="post">
+
+    <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+    <h1 class="h6 mb-3 font-weight-normal"><?= $_GET['errormsg']?></h1>
+
+
+    <input type="acount" id="useraccount" name="username" class="form-control" placeholder="Account name" required="" autofocus="">
+
+    <input type="password" id="inputPassword" name="pwd" class="form-control" placeholder="Password" required="">
+
+    <div class="checkbox mb-3">
+      <label>
+        <input type="checkbox" value="remember-me"> Remember me
+      </label>
     </div>
-  </body>
+    <button class="btn btn-lg btn-primary btn-block" type="submit" name="login_submit">Sign in</button>
+
+  </form>
+  <div class="d-flex justify-content-center links">
+    Don't have an account?<a href="signup.php">Sign Up</a>
+  </div>
+  <p class="mt-5 mb-3 text-muted">© 2020</p>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+
 </html>
--- finalProj/final/signup.php
+++ finalProj/final/signup.php
@@ -4,22 +4,38 @@
     <meta charset="utf-8">
     <meta name="viewport" content="widht=device-width, initial-scale=1">
     <title>My Sign Up</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- Custom styles for this template -->
+    <link href="style.css" rel="stylesheet">
     <!-- Insert Navbar -->
-    <?php require "header.php"; ?>
+    <?php require "header.php";
+
+    ?>
   </head>
-  <body>
-    <div class="container-fluid">
-      <div class="row justify-content-center">
-        <h2>We are signing up.</h2>
-      </div>
-      <div class="row justify-content-center">        
-        <form class="form-signup" action="includes/signup.inc.php" method="post">
-          <input type="text" name="username" placeholder="Username">
-          <input type="text" name="nickname" placeholder="Nickname">
-          <input type="text" name="pwd" placeholder="Password">
-          <button type="submit" name="signup_submit" class="btn btn-dark">SIGN UP</button>
-        </form>
-      </div>
-    </div>
-  </body>
+
+<body class="text-center">
+  <form class="form-signin" action="includes/signup.inc.php" method="post">
+    
+    <h1 class="h3 mb-3 font-weight-normal">Creat an account</h1>
+    <h1 class="h6 mb-3 font-weight-normal"> <?= $_GET['errormsg'] ?></h1>
+    
+
+    <input type="acount" name="username" class="form-control" placeholder="Account name" required="" autofocus="">
+
+    <input type="acount" name="nickname" class="form-control" placeholder="nick name" required="">
+
+    <input type="password" name="pwd" class="form-control" placeholder="Password" required="">
+
+    <input type="password" name="repwd" class="form-control" placeholder="Confirm Password" required="">
+
+
+    <button class="btn btn-lg btn-primary btn-block" type="submit" name="signup_submit">Creat Account</button>
+    <p class="mt-5 mb-3 text-muted">© 2020</p>
+  </form>
+  <?php
+
+  ?>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+
 </html>
